1e07bcd91b0f9994d7bdc6ca10c64300757e5198
gooeyone <luszjo36@gmail.com>
2018-04-03 21:55:19 +0200

    Sigmoid layer cuda sync removed

--- cuda-neural-network/src/layers/sigmoid_activation.cu
+++ cuda-neural-network/src/layers/sigmoid_activation.cu
@@ -6,8 +6,9 @@ __device__ float sigmoid(float x) {
 	return 1.0f / (1 + exp(-x));
 }
 
-__global__ void sigmoid_activation_forward(float* Z, float* A,
-										   int Z_x_dim, int Z_y_dim) {
+__global__ void sigmoidActivationForward(float* Z, float* A,
+										 int Z_x_dim, int Z_y_dim) {
+
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
 
 	if (index < Z_x_dim * Z_y_dim) {
@@ -15,8 +16,9 @@ __global__ void sigmoid_activation_forward(float* Z, float* A,
 	}
 }
 
-__global__ void sigmoid_activation_backprop(float* Z, float* dA, float* dZ,
-											int Z_x_dim, int Z_y_dim) {
+__global__ void sigmoidActivationBackprop(float* Z, float* dA, float* dZ,
+										  int Z_x_dim, int Z_y_dim) {
+
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
 
 	if (index < Z_x_dim * Z_y_dim) {
@@ -24,40 +26,36 @@ __global__ void sigmoid_activation_backprop(float* Z, float* dA, float* dZ,
 	}
 }
 
-SigmoidActivation::SigmoidActivation(std::string name)
-{
+SigmoidActivation::SigmoidActivation(std::string name) {
 	this->name = name;
 }
 
-SigmoidActivation::~SigmoidActivation() { }
+SigmoidActivation::~SigmoidActivation()
+{ }
 
 Matrix SigmoidActivation::forward(Matrix Z) {
-
 	this->Z = Z;
 	A.allocateMemoryIfNotAllocated(Z.shape);
 
 	dim3 block_size(256);
 	dim3 num_of_blocks((Z.shape.y * Z.shape.x + block_size.x - 1) / block_size.x);
 
-	sigmoid_activation_forward<<<num_of_blocks, block_size>>>(Z.data_device.get(), A.data_device.get(),
-														   	  Z.shape.x, Z.shape.y);
-	cudaDeviceSynchronize();
-	NNException::throwIfDeviceErrorsOccurred("Cannot perform sigmoid forward prop.");
+	sigmoidActivationForward<<<num_of_blocks, block_size>>>(Z.data_device.get(), A.data_device.get(),
+														   	Z.shape.x, Z.shape.y);
+	NNException::throwIfDeviceErrorsOccurred("Cannot perform sigmoid forward propagation.");
 
 	return A;
 }
 
 Matrix SigmoidActivation::backprop(Matrix dA, float learning_rate) {
-
 	dZ.allocateMemoryIfNotAllocated(Z.shape);
 
 	dim3 block_size(256);
 	dim3 num_of_blocks((Z.shape.y * Z.shape.x + block_size.x - 1) / block_size.x);
-	sigmoid_activation_backprop<<<num_of_blocks, block_size>>>(Z.data_device.get(), dA.data_device.get(),
-															   dZ.data_device.get(),
-															   Z.shape.x, Z.shape.y);
-	cudaDeviceSynchronize();
-	NNException::throwIfDeviceErrorsOccurred("Cannot perform sigmoid backprop");
+	sigmoidActivationBackprop<<<num_of_blocks, block_size>>>(Z.data_device.get(), dA.data_device.get(),
+															 dZ.data_device.get(),
+															 Z.shape.x, Z.shape.y);
+	NNException::throwIfDeviceErrorsOccurred("Cannot perform sigmoid back propagation");
 
 	return dZ;
 }
