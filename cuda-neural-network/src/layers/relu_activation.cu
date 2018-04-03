96a05c44978c6935ba2e2e50eb2ca9bace60233b
gooeyone <luszjo36@gmail.com>
2018-04-03 21:52:12 +0200

    ReLU layer refactor

--- cuda-neural-network/src/layers/relu_activation.cu
+++ cuda-neural-network/src/layers/relu_activation.cu
@@ -1,8 +1,9 @@
 #include "relu_activation.hh"
 #include "../nn_utils/nn_exception.hh"
 
-__global__ void relu_activation_forward(float* Z, float* A,
-									    int Z_x_dim, int Z_y_dim) {
+__global__ void reluActivationForward(float* Z, float* A,
+									  int Z_x_dim, int Z_y_dim) {
+
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
 
 	if (index < Z_x_dim * Z_y_dim) {
@@ -10,8 +11,9 @@ __global__ void relu_activation_forward(float* Z, float* A,
 	}
 }
 
-__global__ void relu_activation_backprop(float* Z, float* dA, float* dZ,
-											int Z_x_dim, int Z_y_dim) {
+__global__ void reluActivationBackprop(float* Z, float* dA, float* dZ,
+									   int Z_x_dim, int Z_y_dim) {
+
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
 
 	if (index < Z_x_dim * Z_y_dim) {
@@ -24,41 +26,35 @@ __global__ void relu_activation_backprop(float* Z, float* dA, float* dZ,
 	}
 }
 
-ReLUActivation::ReLUActivation(std::string name)
-{
+ReLUActivation::ReLUActivation(std::string name) {
 	this->name = name;
 }
 
 ReLUActivation::~ReLUActivation() { }
 
 Matrix ReLUActivation::forward(Matrix Z) {
-
 	this->Z = Z;
-
 	A.allocateMemoryIfNotAllocated(Z.shape);
 
 	dim3 block_size(256);
 	dim3 num_of_blocks((Z.shape.y * Z.shape.x + block_size.x - 1) / block_size.x);
 
-	relu_activation_forward<<<num_of_blocks, block_size>>>(Z.data_device.get(), A.data_device.get(),
-														   Z.shape.x, Z.shape.y);
-	cudaDeviceSynchronize();
-	NNException::throwIfDeviceErrorsOccurred("Cannot perform ReLU forward prop.");
+	reluActivationForward<<<num_of_blocks, block_size>>>(Z.data_device.get(), A.data_device.get(),
+														 Z.shape.x, Z.shape.y);
+	NNException::throwIfDeviceErrorsOccurred("Cannot perform ReLU forward propagation.");
 
 	return A;
 }
 
 Matrix ReLUActivation::backprop(Matrix dA, float learning_rate) {
-
 	dZ.allocateMemoryIfNotAllocated(Z.shape);
 
 	dim3 block_size(256);
 	dim3 num_of_blocks((Z.shape.y * Z.shape.x + block_size.x - 1) / block_size.x);
-	relu_activation_backprop<<<num_of_blocks, block_size>>>(Z.data_device.get(), dA.data_device.get(),
-															dZ.data_device.get(),
-															Z.shape.x, Z.shape.y);
-	cudaDeviceSynchronize();
-	NNException::throwIfDeviceErrorsOccurred("Cannot perform relu backprop");
+	reluActivationBackprop<<<num_of_blocks, block_size>>>(Z.data_device.get(), dA.data_device.get(),
+													      dZ.data_device.get(),
+														  Z.shape.x, Z.shape.y);
+	NNException::throwIfDeviceErrorsOccurred("Cannot perform ReLU back propagation");
 
 	return dZ;
 }
