fd623c0ac9c79f840f6fa113693ab5c949233600
gooeyone <luszjo36@gmail.com>
2018-04-11 15:48:02 +0200

    Checking accuracy on a test set

--- cuda-neural-network/src/main.cu
+++ cuda-neural-network/src/main.cu
@@ -10,11 +10,13 @@
 
 #include "coordinates_dataset.hh"
 
+float computeAccuracy(const Matrix& predictions, const Matrix& targets);
+
 int main() {
 
 	srand( time(NULL) );
 
-	CoordinatesDataset dataset(100, 20);
+	CoordinatesDataset dataset(100, 21);
 	BCECost bce_cost;
 
 	NeuralNetwork nn;
@@ -23,23 +25,43 @@ int main() {
 	nn.addLayer(new LinearLayer("linear_2", Shape(30, 1)));
 	nn.addLayer(new SigmoidActivation("sigmoid_output"));
 
+	// network training
 	Matrix Y;
-
-	for (int epoch = 0; epoch < 1501; epoch++) {
+	for (int epoch = 0; epoch < 1001; epoch++) {
 		float cost = 0.0;
 
-		for (int batch = 0; batch < dataset.getNumOfBatches(); batch++) {
+		for (int batch = 0; batch < dataset.getNumOfBatches() - 1; batch++) {
 			Y = nn.forward(dataset.getBatches().at(batch));
 			nn.backprop(Y, dataset.getTargets().at(batch));
 			cost += bce_cost.cost(Y, dataset.getTargets().at(batch));
 		}
 
 		if (epoch % 100 == 0) {
-			std::cout 	<< "epoch: " << epoch
-						<< ", cost: " << cost / dataset.getNumOfBatches()
+			std::cout 	<< "Epoch: " << epoch
+						<< ", Cost: " << cost / dataset.getNumOfBatches()
 						<< std::endl;
 		}
 	}
 
+	// compute accuracy
+	Y = nn.forward(dataset.getBatches().at(dataset.getNumOfBatches() - 1));
+	Y.copyDeviceToHost();
+	std::cout 	<< "Accuracy: " << computeAccuracy(Y, dataset.getTargets().at(dataset.getNumOfBatches() - 1))
+				<< std::endl;
+
 	return 0;
+}
+
+float computeAccuracy(const Matrix& predictions, const Matrix& targets) {
+	int m = predictions.shape.x;
+	int correct_predictions = 0;
+
+	for (int i = 0; i < m; i++) {
+		float prediction = predictions[i] > 0.5 ? 1 : 0;
+		if (prediction == targets[i]) {
+			correct_predictions++;
+		}
+	}
+
+	return static_cast<float>(correct_predictions) / m;
 }
